2c80980bcaab120361808cb4bbedfe1fdfdbb717
riyamanasa <72931821+riyamanasa@users.noreply.github.com>
2021-02-11 21:33:18 +0530

    creates years and days to minutes
    3456789 minutes to 6 yeas and 210 days

---
+++ min.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class two {
+    public static void main(String[] Strings) {
+        double minutes = 60 * 24 * 365;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input the number of minutes: ");
+
+        double min = input.nextDouble();
+
+        long years = (long) (min / minutes);
+        int days = (int) (min / 60 / 24) % 365;
+
+        System.out.println((int) min + " minutes is approximately " + years + " years and " + days + " days");
+    }
+}
